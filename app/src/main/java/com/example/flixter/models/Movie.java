e4a1f88d142a7b7e8d09aac84c44432fac9ab617
mparrishFAMU <michael2.parrish@famu.edu>
2021-02-02 23:09:02 -0500

    no problems with code, had to edit Virtual Device to deal with an error caused in API 30.

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,19 +3,31 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+    int movieID;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    public Movie(){
+        //needed for Parcel library
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -30,11 +42,23 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
     }
 
+    public String getBackdropPath(){
+        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+    }
+
     public String getTitle() {
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieID() {
+        return movieID;
     }
 }
